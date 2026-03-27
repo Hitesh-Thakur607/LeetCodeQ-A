947173b5ea102692f50f4501614421a24d3eb293
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-03-27 21:06:42 +0530

    Time: 259 ms (30.04%), Space: 62.1 MB (7.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// vector<int>dp;
+map<pair<int,int>,int>dp;
 int findcount(vector<int>&nums,int tar,int curr,int count,int idx){
     if(curr==tar&&count==nums.size()){
         return 1;
@@ -8,16 +8,19 @@ int findcount(vector<int>&nums,int tar,int curr,int count,int idx){
     if(idx>=nums.size()){
         return 0;
     }
+    if(dp.find({idx,curr})!=dp.end()){
+        return dp[{idx,curr}];
+    }
     if(count>=nums.size()){
         return 0;
     }
     int minus=findcount(nums,tar,curr-nums[idx],count+1,idx+1);
     int plus=findcount(nums,tar,curr+nums[idx],count+1,idx+1);
     // cout<<minus+plus<<endl;
-    return minus+plus;
+    return dp[{idx,curr}]=minus+plus;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        // dp.resize(nums.size()+1,-1);
+         dp.clear();
         return findcount(nums,target,0,0,0);
     }
 };
