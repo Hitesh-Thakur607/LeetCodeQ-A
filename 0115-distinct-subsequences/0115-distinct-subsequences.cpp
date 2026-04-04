f356051074ed428081b1200023aa1481e582f836
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-04-04 13:08:46 +0530

    Time: 43 ms (15.21%), Space: 44.3 MB (33.44%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<long long>> dp;
+    long long disti(string &s, string &t, int i, int j) {
+        if (j == t.size()) return 1;
+        if (i == s.size()) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        long long one = 0, two = 0;
+        if (s[i] == t[j]) {
+            one = disti(s, t, i+1, j+1);
+        }
+        two = disti(s, t, i+1, j);
+        return dp[i][j] = one + two;
+    }
+    int numDistinct(string s, string t) {
+        dp.assign(s.size(), vector<long long>(t.size(), -1));
+        return disti(s, t, 0, 0);
+    }
+};
