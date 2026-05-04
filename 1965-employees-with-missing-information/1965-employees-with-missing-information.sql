b1ea201a66d111f12106f330fb9f293ead2ff4c2
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-05-04 12:06:32 +0530

    Time: 655 ms (14.68%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -4,7 +4,7 @@ LEFT JOIN Salaries s ON e.employee_id = s.employee_id
 WHERE s.employee_id IS NULL
 UNION
 SELECT s.employee_id
-FROM Salaries s
-LEFT JOIN Employees e ON s.employee_id = e.employee_id
+FROM Employees e
+RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
 WHERE e.employee_id IS NULL
 ORDER BY employee_id ASC;
