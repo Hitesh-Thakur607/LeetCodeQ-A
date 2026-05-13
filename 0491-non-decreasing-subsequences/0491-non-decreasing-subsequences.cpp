1182d4d398b48fa866639bbb95d21d90ce54af63
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-05-14 00:42:16 +0530

    Time: 31 ms (60.12%), Space: 29.8 MB (46.68%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-set<vector<int>>arr;
-void subsq(vector<int>&nums,vector<int>&temp,int lastval,int index){
-    if(temp.size()>=2){
-        if(arr.find(temp)==arr.end()){
-            arr.insert(temp);
+    vector<vector<int>> ans;
+    void subsq(vector<int>& nums, vector<int>& temp, int index) {
+        if (temp.size() >= 2) {
             ans.push_back(temp);
         }
-    }
-    if(index>=nums.size()){
-        return ;
-    }
-    for(int i=index;i<nums.size();i++){
-        if(lastval<=nums[i]||lastval==INT_MIN){
+        unordered_set<int> used; 
+        for (int i = index; i < nums.size(); i++) {
+            if ((!temp.empty() && nums[i] < temp.back()) || used.count(nums[i])) {
+                continue;
+            }
+            used.insert(nums[i]);
             temp.push_back(nums[i]);
-            subsq(nums,temp,nums[i],i+1);
+            subsq(nums, temp, i + 1);
             temp.pop_back();
         }
     }
-}
+
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<int>temp;
-        subsq(nums,temp,INT_MIN,0);
+        vector<int> temp;
+        subsq(nums, temp, 0);
         return ans;
-    }   
+    }
 };
