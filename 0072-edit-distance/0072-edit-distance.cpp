316a8b6e262c0de5b31f3dbcea7902f00dd6f0f4
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-05-31 19:27:35 +0530

    Time: 6 ms (70.46%), Space: 13.2 MB (39.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
 vector<vector<int>>dp;
 int mini(string &word1,string &word2,int i,int j){
-        if(i == word1.size()) return word2.size() - j;
+       if(i == word1.size()) return word2.size() - j;
         if(j == word2.size()) return word1.size() - i;
-
     if(dp[i][j]!=-1){
         return dp[i][j];
     }
     if(word1[i]==word2[j]){
-        return dp[i][j]=mini(word1,word2,i+1,j+1);
+        return mini(word1,word2,i+1,j+1);
     }
     return dp[i][j]=1+min(min(mini(word1,word2,i+1,j),mini(word1,word2,i,j+1)),mini(word1,word2,i+1,j+1));
 }
     int minDistance(string word1, string word2) {
-        dp.resize(501,vector<int>(501,-1));
+        int maxii=max(word1.size(),word2.size());
+        dp.assign(maxii,vector<int>(maxii,-1));
         return mini(word1,word2,0,0);
     }
 };
