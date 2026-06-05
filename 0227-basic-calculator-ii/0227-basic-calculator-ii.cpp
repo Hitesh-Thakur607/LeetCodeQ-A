3018a207c17fb594eada8c11ff0e7ae0e1dd528a
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-06-05 12:02:24 +0530

    Time: 7 ms (61.76%), Space: 15.3 MB (42.55%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,95 +1,41 @@
 class Solution {
 public:
-    int calculate(string sa) {
-
-        string s = "";
-
-        for(int j = 0; j < sa.size(); j++){
-            if(sa[j] != ' '){
-                s += sa[j];
+    int calculate(string s) {
+        stack<int>ans;
+        char sign='+';
+        int num=0;
+        int result=0;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                num=num*10+(s[i]-'0');
             }
-        }
-
-        stack<long long> val;
-        stack<char> opp;
-
-        int i = 0;
-
-        while(i < s.size()){
-
-            // build complete number
-            long long count = 0;
-
-            while(i < s.size() && isdigit(s[i])){
-                count = count * 10 + (s[i] - '0');
-                i++;
-            }
-
-            // if number found
-            if(count || (i > 0 && s[i-1] == '0')){
-
-                // handle * and /
-                if(!opp.empty() && (opp.top() == '*' || opp.top() == '/')){
-
-                    char op = opp.top();
-                    opp.pop();
-
-                    long long top = val.top();
-                    val.pop();
-
-                    if(op == '*'){
-                        val.push(top * count);
-                    }
-                    else{
-                        val.push(top / count);
-                    }
+            if(!isdigit(s[i])&&s[i]!=' '&&i>0||i == s.size() - 1){
+                if(sign=='+'){
+                    ans.push(num);
                 }
-                else{
-                    val.push(count);
+                if(sign=='-'){
+                    ans.push(-num);
                 }
-            }
-
-            if(i < s.size()){
-                opp.push(s[i]);
-            }
-
-            i++;
-        }
-
-        // now solve + and -
-        stack<long long> temp;
-
-        while(!val.empty()){
-            temp.push(val.top());
-            val.pop();
-        }
-
-        stack<char> tempOp;
-
-        while(!opp.empty()){
-            tempOp.push(opp.top());
-            opp.pop();
-        }
-
-        long long ans = temp.top();
-        temp.pop();
-
-        while(!tempOp.empty()){
-
-            char op = tempOp.top();
-            tempOp.pop();
-
-            long long num = temp.top();
-            temp.pop();
-
-            if(op == '+'){
-                ans += num;
-            }
-            else if(op == '-'){
-                ans -= num;
+                if(sign=='*'){
+                    int val=ans.top();
+                    ans.pop();
+                    ans.push(num*val);
+                }
+                if(sign=='/'){
+                    int val=ans.top();
+                    ans.pop();
+                    ans.push(val/num);
+                }
+                sign=s[i];
+                num=0;
             }
         }
-
-        return ans;
+        // ans.push(num);
+        while(!ans.empty()){
+            result+=ans.top();
+            ans.pop();
+        }
+        return result;
     }
 };
+
