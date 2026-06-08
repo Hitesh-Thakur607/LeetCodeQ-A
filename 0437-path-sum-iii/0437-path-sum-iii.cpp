8212768ad0f90669c4ff5144df2abc063b2da53e
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-06-08 14:01:21 +0530

    Time: 22 ms (11.55%), Space: 18.9 MB (89.9%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,25 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int count = 0;
-
-    void allpathsum(TreeNode* root, long long sum, int target) {
-        if (!root) return;
-
-        sum += root->val;
-
-        if (sum == target) count++;
-
-        allpathsum(root->left, sum, target);
-        allpathsum(root->right, sum, target);
-    }
-
-    int pathSum(TreeNode* root, int targetSum) {
-        if (!root) return 0;
-
-        allpathsum(root, 0, targetSum);         
-        pathSum(root->left, targetSum);         
-        pathSum(root->right, targetSum);       
-
+    int countpath(TreeNode* root,int tar,long long sum){
+        if(root==nullptr){
+            return 0;
+        }
+        int count=0;
+        sum+=root->val;
+        count+=countpath(root->left,tar,sum);
+        count+=countpath(root->right,tar,sum);
+        if(sum==tar){
+            count++;
+        }
         return count;
     }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root==nullptr){
+            return 0;
+        }
+        int count=0;
+       count+=countpath(root,targetSum,0);
+       count+=pathSum(root->left,targetSum);
+       count+=pathSum(root->right,targetSum);
+       return count;
+    }   
 };
