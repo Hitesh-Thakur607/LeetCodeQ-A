0ac317702eae9d2992edf5df96b5431a5b37d048
Hitesh Thakur <157588459+Hitesh-Thakur607@users.noreply.github.com>
2026-07-03 16:56:30 +0530

    Time: 4 ms (35.08%), Space: 9.8 MB (49.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>alpha(26,0);
+        for(int i=0;i<s.size();i++){
+            alpha[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+            alpha[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(alpha[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
